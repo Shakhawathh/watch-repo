5d5844d865203212c2e525f9db31df4f7df66aa0
itachiafif <mohammmadafif710@gmail.com>
2022-04-05 23:56:15 +0600

    last

--- src/components/Customer/Customer.js
+++ src/components/Customer/Customer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Customer.css';
 const Customer = (props) => {
-    const{name, price,ratings,picture} = props.watch;
+    const { name, review, ratings, picture } = props.watch;
     return (
         <div className='customer'>
-             <img src={picture} alt=""></img>
+            <img src={picture} alt=""></img>
             <div>
                 <h3>name:{name}</h3>
-                <h4>price:${price}</h4>
+                <h4>{review}</h4>
                 <p><b>ratings:{ratings}</b></p>
             </div>
         </div>
